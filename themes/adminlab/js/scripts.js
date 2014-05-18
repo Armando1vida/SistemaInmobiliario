91e3f558e2542d0b38c4de0482dac6ec8e8f77e6
ArmandoPC <armand1live@gmail.com>
2014-05-17 19:31:06 -0500

    Carde de tema

--- themes/adminlab/js/scripts.js
+++ themes/adminlab/js/scripts.js
@@ -299,78 +299,78 @@ var App = function () {
         addEvent("My Event 9", "success");
         addEvent("My Event 10", "default");
 
-//        $('#calendar').fullCalendar({
-//            header: h,
-//            editable: true,
-//            droppable: true, // this allows things to be dropped onto the calendar !!!
-//            drop: function (date, allDay) { // this function is called when something is dropped
-//
-//                // retrieve the dropped element's stored Event Object
-//                var originalEventObject = $(this).data('eventObject');
-//                // we need to copy it, so that multiple events don't have a reference to the same object
-//                var copiedEventObject = $.extend({}, originalEventObject);
-//
-//                // assign it the date that was reported
-//                copiedEventObject.start = date;
-//                copiedEventObject.allDay = allDay;
-//                copiedEventObject.className = $(this).attr("data-class");
-//
-//                // render the event on the calendar
-//                // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
-//                $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
-//
-//                // is the "remove after drop" checkbox checked?
-//                if ($('#drop-remove').is(':checked')) {
-//                    // if so, remove the element from the "Draggable Events" list
-//                    $(this).remove();
-//                }
-//            },
-//            events: [{
-//                title: 'All Day Event',
-//                start: new Date(y, m, 1),
-//                className: 'label label-default',
-//            }, {
-//                title: 'Long Event',
-//                start: new Date(y, m, d - 5),
-//                end: new Date(y, m, d - 2),
-//                className: 'label label-success',
-//            }, {
-//                id: 999,
-//                title: 'Repeating Event',
-//                start: new Date(y, m, d - 3, 16, 0),
-//                allDay: false,
-//                className: 'label label-default',
-//            }, {
-//                id: 999,
-//                title: 'Repeating Event',
-//                start: new Date(y, m, d + 4, 16, 0),
-//                allDay: false,
-//                className: 'label label-important',
-//            }, {
-//                title: 'Meeting',
-//                start: new Date(y, m, d, 10, 30),
-//                allDay: false,
-//                className: 'label label-info',
-//            }, {
-//                title: 'Lunch',
-//                start: new Date(y, m, d, 12, 0),
-//                end: new Date(y, m, d, 14, 0),
-//                allDay: false,
-//                className: 'label label-warning',
-//            }, {
-//                title: 'Birthday Party',
-//                start: new Date(y, m, d + 1, 19, 0),
-//                end: new Date(y, m, d + 1, 22, 30),
-//                allDay: false,
-//                className: 'label label-success',
-//            }, {
-//                title: 'Click for Google',
-//                start: new Date(y, m, 28),
-//                end: new Date(y, m, 29),
-//                url: 'http://google.com/',
-//                className: 'label label-warning',
-//            }]
-//        });
+        $('#calendar').fullCalendar({
+            header: h,
+            editable: true,
+            droppable: true, // this allows things to be dropped onto the calendar !!!
+            drop: function (date, allDay) { // this function is called when something is dropped
+
+                // retrieve the dropped element's stored Event Object
+                var originalEventObject = $(this).data('eventObject');
+                // we need to copy it, so that multiple events don't have a reference to the same object
+                var copiedEventObject = $.extend({}, originalEventObject);
+
+                // assign it the date that was reported
+                copiedEventObject.start = date;
+                copiedEventObject.allDay = allDay;
+                copiedEventObject.className = $(this).attr("data-class");
+
+                // render the event on the calendar
+                // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
+                $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
+
+                // is the "remove after drop" checkbox checked?
+                if ($('#drop-remove').is(':checked')) {
+                    // if so, remove the element from the "Draggable Events" list
+                    $(this).remove();
+                }
+            },
+            events: [{
+                title: 'All Day Event',
+                start: new Date(y, m, 1),
+                className: 'label label-default',
+            }, {
+                title: 'Long Event',
+                start: new Date(y, m, d - 5),
+                end: new Date(y, m, d - 2),
+                className: 'label label-success',
+            }, {
+                id: 999,
+                title: 'Repeating Event',
+                start: new Date(y, m, d - 3, 16, 0),
+                allDay: false,
+                className: 'label label-default',
+            }, {
+                id: 999,
+                title: 'Repeating Event',
+                start: new Date(y, m, d + 4, 16, 0),
+                allDay: false,
+                className: 'label label-important',
+            }, {
+                title: 'Meeting',
+                start: new Date(y, m, d, 10, 30),
+                allDay: false,
+                className: 'label label-info',
+            }, {
+                title: 'Lunch',
+                start: new Date(y, m, d, 12, 0),
+                end: new Date(y, m, d, 14, 0),
+                allDay: false,
+                className: 'label label-warning',
+            }, {
+                title: 'Birthday Party',
+                start: new Date(y, m, d + 1, 19, 0),
+                end: new Date(y, m, d + 1, 22, 30),
+                allDay: false,
+                className: 'label label-success',
+            }, {
+                title: 'Click for Google',
+                start: new Date(y, m, 28),
+                end: new Date(y, m, 29),
+                url: 'http://google.com/',
+                className: 'label label-warning',
+            }]
+        });
 
     }
 
@@ -2314,7 +2314,7 @@ try{
 
 //knob
 
-//$(".knob").knob();
+$(".knob").knob();
 
 
 
