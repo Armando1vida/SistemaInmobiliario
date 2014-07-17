a7b5ef638138f0ad06b8394961cf2a6f38b3b6a4
ArmandoPC <armand1live@gmail.com>
2014-07-17 00:47:17 -0500

    Change on module clientes and inmuebles

--- protected/extensions/Truulo/views/truuloModuleSearch.php
+++ protected/extensions/Truulo/views/truuloModuleSearch.php
@@ -4,18 +4,18 @@
     /** @var AweActiveForm $form */
     Yii::app()->clientScript->registerScript('search' . $this->id, "
     $('#" . $this->nameInput . "').keyup(function(){
-//        $('#".$this->grid_id."').attr('class','grid-view-loading');
+//        $('#" . $this->grid_id . "').attr('class','grid-view-loading');
         $.fn.yiiGridView.update('" . $this->grid_id . "',{
             data:$('#" . $this->id . "').serialize(),
             complete:function(jqXHR, status) {
                     if (status=='success'){
-                        console.log('completo');
+//                        console.log('completo');
                     }
                 },
             beforeSend:function() {
-//                var settings= $.fn.yiiGridView.gridSettings['".$this->grid_id."'];
-//                    $('#".$this->grid_id."').addClass(settings.loadingClass);
-                    console.log('enviado');
+//                var settings= $.fn.yiiGridView.gridSettings['" . $this->grid_id . "'];
+//                    $('#" . $this->grid_id . "').addClass(settings.loadingClass);
+//                    console.log('enviado');
                 },
             
           });
@@ -45,16 +45,16 @@
             </button>
             <ul class="truulo-search-options" id="<?php echo $this->idList; ?>">
                 <li><input type="checkbox" name="search[filters][]" value="all" class="all" checked="checked" /> <?php echo Yii::t('app', 'Todo') ?></li>
- 
-                <?php if($this->modulo=="campaniaOperador"):?>
- <?php foreach ($this->model->searchParams(true) as $param): ?>
-                    <li><input type="checkbox" name="search[filters][]" value="<?php echo $param ?>" /> <?php echo $this->model->getAttributeLabel($param) ?></li>
-                <?php endforeach; ?>
-                    <?php else: ?>
+
+                <?php if ($this->modulo == "campaniaOperador"): ?>
+                    <?php foreach ($this->model->searchParams(true) as $param): ?>
+                        <li><input type="checkbox" name="search[filters][]" value="<?php echo $param ?>" /> <?php echo $this->model->getAttributeLabel($param) ?></li>
+                    <?php endforeach; ?>
+                <?php else: ?>
                     <?php foreach ($this->model->searchParams() as $param): ?>
-                    <li><input type="checkbox" name="search[filters][]" value="<?php echo $param ?>" /> <?php echo $this->model->getAttributeLabel($param) ?></li>
-                <?php endforeach; ?>
-                    <?php endif;?>
+                        <li><input type="checkbox" name="search[filters][]" value="<?php echo $param ?>" /> <?php echo $this->model->getAttributeLabel($param) ?></li>
+                    <?php endforeach; ?>
+                <?php endif; ?>
             </ul>
         </div>
         <input class="span12 search_value" id="<?php echo $this->nameInput ?>" name="search[value]" type="text" autocomplete="off">
--- protected/modules/inmuebles/views/inmueble/galery.php
+++ protected/modules/inmuebles/views/inmueble/galery.php
@@ -7,15 +7,13 @@
 ?>
 <div class="well"> 
     <div class="row-fluid">
-
-
         <div class="span3">
-            <div class="pricing-table">
+            <div class="pricing-table ">
                 <div class="pricing-head">
                     <h3> 0000011 </h3>
                     <img data-src="holder.js/300x200" alt="300x200" src="/SistemaInmobiliario/uploads/inmueble/7/1405554946952421327.jpg"  style="width: 300px; height: 150px;">
                 </div>
-                <ul class="unstyled">
+                <ul >
                     <li><strong>Free</strong> setup</li>
                     <li><strong>10</strong> Website</li>
                     <li><strong>20</strong> Projects</li>
